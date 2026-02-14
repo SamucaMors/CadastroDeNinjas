5bfbed49b01c4167130195a2ae134a60b0957964
Samuel <samuca.mors@gmail.com>
2026-02-14 15:10:31 -0300

    refactor: rename MissoesRepository to MissaoRepository

--- CadastroDeNinjas/src/main/java/dev/samcode/CadastroDeNinjas/missoes/repository/MissaoRepository.java
+++ CadastroDeNinjas/src/main/java/dev/samcode/CadastroDeNinjas/missoes/repository/MissaoRepository.java
@@ -4,5 +4,5 @@ import dev.samcode.CadastroDeNinjas.missoes.model.Missao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissoesRepository extends JpaRepository<Missao, Long> {
+public interface MissaoRepository extends JpaRepository<Missao, Long> {
 }
